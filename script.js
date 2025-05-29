ddbb1e7d42f8cb413f118ac4c2e9597dd4fc326d
gon-cab <gnzcabs@gmail.com>
2025-05-29 16:58:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const API_VERSION = "2023-10-01";
 const query = encodeURIComponent('*[_type == "project"]{title, description, "imageUrl": image.asset->url}');
 const url = `https://80k5e13a.api.sanity.io/v2025-05-29/data/query/production?query=&perspective=published`;
 
-fetch('https://80k5e13a.api.sanity.io/v2025-05-29/data/query/production?query=&perspective=published')
+fetch('https://80k5e13a.api.sanity.io/v2025-05-29/data/query/production?query=*%5B_type+%3D%3D+%22project%22%5D%7B%0A++title%2C%0A++description%2C%0A++%22imageUrl%22%3A+image.asset-%3Eurl%0A%7D&perspective=published')
   .then(res => res.json())
   .then(({ result }) => {
     const container = document.getElementById("projects");

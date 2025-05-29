15c2bbb28b5c7407719d25c3c19d84297e5baf6d
gon-cab <gnzcabs@gmail.com>
2025-05-29 17:32:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const DATASET = "production";
 const API_VERSION = "2023-10-01";
 
 const QUERY = encodeURIComponent(`*[_type == "project"]{title, description, "imageUrl": image.asset->url}`);
-const url = 'https://${PROJECT_ID}.api.sanity.io/v${API_VERSION}/data/query/${DATASET}?query=${QUERY}&perspective=published';
+const url = '${PROJECT_ID}.api.sanity.io/v${API_VERSION}/data/query/${DATASET}?query=${QUERY}&perspective=published';
 
-fetch(https://80k5e13a.api.sanity.io/v2025-05-29/data/query/production?query=*%5B_type+%3D%3D+%22project%22%5D%7B%0A++title%2C%0A++description%2C%0A++%22imageUrl%22%3A+image.asset-%3Eurl%0A%7D&perspective=published)
+fetch(url)
   .then(res => res.json())
   .then(({ result }) => {
     const container = document.getElementById("projects");
